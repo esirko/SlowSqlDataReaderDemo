15670a518c4ad87d3989d0e383796ab2eec6bff9
Edwin Sirko <edsirko@microsoft.com>
2021-02-09 11:21:47 -0500

    dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SlowSqlDataReaderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
